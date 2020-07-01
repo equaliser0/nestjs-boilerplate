2e9618e5f9726c266941614fb6d3b7271ca6b823
equaliser0 <accounts@equaliser.minimalcity.de>
2020-07-01 12:19:58 +0200

    Prepare MongoDB

--- src/database-connection.service.ts
+++ src/database-connection.service.ts
@@ -5,17 +5,26 @@ import { TypeOrmOptionsFactory, TypeOrmModuleOptions } from '@nestjs/typeorm';
 export class DatabaseConnectionService implements TypeOrmOptionsFactory {
   createTypeOrmOptions(): TypeOrmModuleOptions {
     return {
-      name: 'default',
-      type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: Number(process.env.DATABASE_PORT),
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_DB,
-      synchronize: true,
-      dropSchema: true,
-      logging: true,
-      entities: ['dist/**/*.entity.js'],
+      type: "mongodb",
+      url: "mongodb+srv://equaliser:RFZu7AO98tP9jW2r@nestjsrealworld-j72og.mongodb.net/test?retryWrites=true&w=majority",
+      database: "test",
+      entities: ["src/**/**.entity{.ts,.js}"],
+      synchronize: false,
+      useNewUrlParser: true,
+      useUnifiedTopology: true
     };
+    // {
+    //   name: 'default',
+    //   type: 'postgres',
+    //   host: process.env.DATABASE_HOST,
+    //   port: Number(process.env.DATABASE_PORT),
+    //   username: process.env.DATABASE_USER,
+    //   password: process.env.DATABASE_PASSWORD,
+    //   database: process.env.DATABASE_DB,
+    //   synchronize: true,
+    //   dropSchema: true,
+    //   logging: true,
+    //   entities: ['dist/**/*.entity.js'],
+    // };
   }
 }
